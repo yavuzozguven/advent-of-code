42bf1a163d495a779075c8ddd6aeb2d798e79b5e
Yavuz Özgüven <yavuzozguven@hotmail.com>
2021-12-02 11:28:12 +0300

    fc

--- 2021/Day 1/src/main/scala/Main.scala
+++ 2021/Day 1/src/main/scala/Main.scala
@@ -2,12 +2,20 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 
 object Main {
+
+  var list:List[Int] = null
+
   def main(args: Array[String]): Unit = {
     val path = Paths.get("input.txt")
     val str = new String(Files.readAllBytes(path), StandardCharsets.UTF_8)
 
-    val list = str.split("\n").map(a=>a.trim.toInt).toList
+    list = str.split("\n").map(a=>a.trim.toInt).toList
 
+    part1
+    part2
+  }
+
+  def part1(): Unit = {
     var i = 0
     var cnt = 0
     while(i<list.length-1){
@@ -15,8 +23,19 @@ object Main {
         cnt += 1
       i += 1
     }
-
     println(cnt)
+  }
 
+  def part2(): Unit = {
+    var i = 0
+    var cnt = 0
+    while(i<list.length-3){
+      val sum1 = list(i) + list(i+1) + list(i+2)
+      val sum2 = list(i+1) + list(i+2) + list(i+3)
+      if(sum2>sum1)
+        cnt += 1
+      i += 1
+    }
+    println(cnt)
   }
 }
